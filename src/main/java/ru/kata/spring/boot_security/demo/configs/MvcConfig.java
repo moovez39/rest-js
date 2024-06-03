71089d966d48cb7ef0e7c9e147bad9ba2a4cf759
Dmitriy Plashchenkov <supergogi39@yandex.ru>
2024-06-03 23:41:25 +0300

    work in progress

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -1,13 +1,15 @@
 package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    public void addViewControllers(ViewControllerRegistry registry) {
+    public void addViewControllers(ViewControllerRegistry registry, ResourceHandlerRegistry resourceHandlerRegistry) {
         registry.addViewController("/user").setViewName("view/user");
         registry.addViewController("/login").setViewName("view/login");
+        resourceHandlerRegistry.addResourceHandler("/static/");
     }
 }
